79cf2da5b429cc3575e54324f2b77359292ae82a
Andris <andris.enins@datoriki.lv>
2014-03-15 15:59:59 +0200

    stuff

--- skripts.js
+++ skripts.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-	$('#stuff').hide(0).delay( 4600 ).show(0);
-	$('#splashScreen').show(0).delay(4200).fadeOut(400);
+	$('#stuff').hide(0).delay( 8600 ).show(0);
+	$('#splashScreen').show(0).delay(8200).fadeOut(400);
 });
